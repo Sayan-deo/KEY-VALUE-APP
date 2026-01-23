53f3e304b128ba751804647e9a53c1bf55c8335b
sayan-deo <sayandeogharia39@gmail.com>
2026-01-23 19:03:58 +0530

    Third Commit

--- start-db.sh
+++ start-db.sh
@@ -11,12 +11,24 @@ KEY_VALUE_DB="Key-value-db"
 KEY_VALUE_USER="Key-value-user"
 KEY_VALUE_PASSWORD="Key-value-password"
 
+# Connectivity
+LOCALHOST_PORT=27017
+CONTAINER_PORT=27017
+NETWORK_NAME="key-value-net"
+
+# Storage
+VOLUME_NAME="key-value-data"
+VOLUME_CONTAINER_PATH=/data/db
+
 docker run --rm -d --name $CONTAINER_NAME \
     -e MONGODB_INITDB_ROOT_USERNAME=$ROOT_USER \
     -e MONGODB_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
     -e KEY_VALUE_DB=$KEY_VALUE_DB \
     -e KEY_VALUE_USER=$KEY_VALUE_USER \
     -e KEY_VALUE_PASSWORD=$KEY_VALUE_PASSWORD \
+    -p $LOCALHOST_PORT:$CONTAINER_PORT \
+    -v $VOLUME_NAME:$VOLUME_CONTAINER_PATH \
     -v ./db-config/mongodb-init.js:/docker-entrypoint-initdb.d/mongodb-init.js:ro \
+    --network $NETWORK_NAME \
     $MONGODB_NAME:$MONGODB_TAG
     
